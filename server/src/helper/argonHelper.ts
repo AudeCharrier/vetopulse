77f1bd085c4700cb00bab90190c84890f8ff7e9a
AUDE CHARRIER <charrier.aude@gmail.com>
2026-07-15 16:46:46 +0200

    feat:hash password with argon2

---
+++ server/src/helper/argonHelper.ts
@@ -0,0 +1,19 @@
+import argon2 from "argon2";
+
+const hashingOptions = {
+	type: argon2.argon2id,
+	memoryCost: 2 ** 16,
+	timeCost: 5,
+	parallelism: 1,
+};
+
+const hashPassword = (plainPassword: string): Promise<string> =>
+	argon2.hash(plainPassword, hashingOptions);
+
+// /!\ ordre des arguments d'argon2.verify : (hash, plain)
+const verifyPassword = (
+	hash: string,
+	plainPassword: string,
+): Promise<boolean> => argon2.verify(hash, plainPassword);
+
+export { hashPassword, verifyPassword };
