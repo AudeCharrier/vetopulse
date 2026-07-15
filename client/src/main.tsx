5ee9864de17e3b6a76f5b34a3c0eaa1e4add0190
AUDE CHARRIER <charrier.aude@gmail.com>
2026-07-15 09:59:14 +0200

    feat:router with LoginPage (basic)

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,10 +1,24 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router";
+import LoginPage from "./pages/LoginPage";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+import "./index.css";
+import App from "./App.tsx";
+
+const router = createBrowserRouter([
+	{
+		element: <App />,
+		children: [
+			{
+				path: "/",
+				element: <LoginPage />,
+			},
+		],
+	},
+]);
+
+const rootElement = document.getElementById("root");
+
+if (rootElement != null) {
+	ReactDOM.createRoot(rootElement).render(<RouterProvider router={router} />);
+}
---
+++ client/src/pages/LoginPage.tsx
@@ -0,0 +1,5 @@
+function LoginPage() {
+	return <p>hello from login</p>;
+}
+
+export default LoginPage;
